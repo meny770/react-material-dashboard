b18c3f48e505478adac5b273522a9df0e0ad5b6a
meny770 <menachem1414@gmail.com>
2020-11-26 11:43:17 +0200

    save

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
 import TopBar from './TopBar';
 
@@ -38,7 +37,6 @@ const MainLayout = () => {
       <div className={classes.wrapper}>
         <div className={classes.contentContainer}>
           <div className={classes.content}>
-            <Outlet />
           </div>
         </div>
       </div>
